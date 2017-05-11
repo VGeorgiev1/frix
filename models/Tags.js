ef7ffba886024d75e2c1f77a1cd12d36d06fc2c6
NotGayBut5CentsAre5Cents <telerik007@gmail.com>
2017-05-11 18:35:05 +0300

    Tags init

--- models/Tags.js
+++ models/Tags.js
@@ -4,9 +4,46 @@ const ObjectId = mongoose.Schema.Types.ObjectId;
 let tagsSchema = mongoose.Schema({
         title: {type: String},
         description: {type: String},
-        picture: {type: String}
 });
 
 const Tags = mongoose.model('Tags', tagsSchema);
 
 module.exports = Tags;
+
+var listOfTagsName = [
+        "Боклyци", 
+        "Коли", 
+        "Подобрения", 
+        "Светофар", 
+        "Пешеходни", 
+        "Тротоар", 
+        "Селски", 
+        "Басейн", 
+        "Дърво", 
+        "Пейки", 
+        "Осветление", 
+        "Сгради", 
+        "Кошче", 
+        "Дупки", 
+        "Шахти", 
+        "Предпазители", 
+        "Пътни знаци", 
+        "Язовир", 
+        "Боядисване", 
+        "Навеси"
+];
+
+
+module.exports.initialize = () => {
+        listOfTagsName.forEach(function(name) {   
+                Tags.findOne({title:  name}).then(tags =>{
+                if(!tags){
+                        let tag = {
+                                title: name,
+                                description: "Проблем със боклуци"
+                        }
+                        Tags.create(tag);
+                }
+                });
+        });
+}
